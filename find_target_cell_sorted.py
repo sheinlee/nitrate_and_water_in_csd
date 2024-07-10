75926f460fa016df71225922dc5b776ef05234cc
sheinlee <shichengli97@gmail.com>
2024-07-10 13:24:15 -0500

    cell

--- find_target_cell_sorted.py
+++ find_target_cell_sorted.py
@@ -9,17 +9,20 @@ start = process_time()
 
 cry_reader = CrystalReader('CSD')
 
-# 目标晶胞参数
-target_lengths = [8.2115, 10.1867, 11.6511]
-target_angles = [114.078, 98.370, 99.812]
+# 目标晶胞参数（长度按从小到大排列）
+target_lengths = sorted([8.2115, 10.1867, 11.6511])
+# 目标角度参数（角度按从小到大排列）
+target_angles = sorted([114.078, 98.370, 99.812])
 
 # 允许误差范围
-tolerance_length_less_accurate = 0.1
+tolerance_length_less_accurate = 1
 tolerance_length_more_accurate = 0.0001
-tolerance_angle_less_accurate = 0.1
+tolerance_angle_less_accurate = 1
 tolerance_angle_more_accurate = 0.001
-
+count=0
 for cry in cry_reader:
+    count+=1
+    print(count)
     print(cry.identifier)
     lengths = sorted(cry.cell_lengths)
     angles = sorted(cry.cell_angles)
