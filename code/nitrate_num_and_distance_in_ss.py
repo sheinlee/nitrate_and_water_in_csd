dc7cdab416b191510904c117df2f5d0a62511e14
sheinlee <shichengli97@gmail.com>
2024-08-08 13:02:30 -0500

    nitrate num distance

---
+++ code/nitrate_num_and_distance_in_ss.py
@@ -0,0 +1,136 @@
+#this script is used to find how many and how far those nitrates in second shell
+#file path =  data/subset3/merged_nitrate_hits.txt
+
+from re import T
+from attr import has
+from ccdc import io 
+from ccdc.io import MoleculeReader, CrystalReader,EntryReader
+import math
+import numpy as np
+import pandas as pd
+from itertools import compress
+import os
+from time import process_time
+
+def is_nitrate(atomN):
+    flag = False
+    if all(atom.atomic_symbol == 'O' for atom in atomN.neighbours) and len(atomN.neighbours) == 3:
+        flag = True
+
+    return flag
+
+def distance_calculation(ln_coordinates_list, N_coordinates_list):
+    distance_list = []
+    
+    for lncrd in ln_coordinates_list:
+        ln_x = lncrd.x
+        ln_y = lncrd.y
+        ln_z = lncrd.z
+        
+        for Ncrd in N_coordinates_list:
+            N_x = Ncrd.x
+            N_y = Ncrd.y
+            N_z = Ncrd.z
+            
+            # Calculate the Euclidean distance
+            distance = math.sqrt((ln_x - N_x)**2 + (ln_y - N_y)**2 + (ln_z - N_z)**2)
+            
+            # Append the distance to the distance_list
+            distance_list.append(distance)
+    
+    return distance_list
+
+def nitrate_num_and_distance_in_ss(ELEMENT):
+    filepath = 'data/subset3/merged_nitrate_hits.txt'
+    mol_reader = MoleculeReader(filepath, format='identifiers')
+    countm = 0
+    for mol in mol_reader:
+        try:
+            mol.assign_bond_types()
+            mol.add_hydrogens()
+        except RuntimeError as e:
+            print(f"Error adding hydrogens to molecule {mol.identifier}: {e}")
+            continue
+        
+        lncrds = []
+        Ncrds = []
+        has_element = False
+        for atom in mol.atoms:
+                if atom.atomic_symbol == ELEMENT:
+                    has_element = True
+                    lncrd = atom.coordinates
+                    lncrds.append(lncrd)
+        for com in mol.components:
+            if any(atom.atomic_symbol == ELEMENT for atom in com.atoms): continue 
+            if has_element:
+                for atom in com.atoms:
+                    if atom.atomic_symbol == 'N':
+                        
+                        if is_nitrate(atom):
+                            Ncrd = atom.coordinates #Ncrd.x, Ncrd.y, Ncrd.z
+                            Ncrds.append(Ncrd)
+        dis_list = distance_calculation(lncrds,Ncrds)
+        if len(dis_list)>0:
+            print(mol.identifier) 
+            print(dis_list)
+            for com in mol.components:
+                print(com.atoms)
+
+        if countm ==3: break
+        countm += 1
+    
+    
+    # countc = 0
+    # cry_reader = CrystalReader(filepath, format='identifiers')
+    # for cry in cry_reader:
+    #     asy_cry_name = cry.asymmetric_unit_molecule
+    #     lncrds = []
+    #     Ncrds = []
+    #     has_element = False
+    #     for atom in asy_cry_name.atoms:
+    #             if atom.atomic_symbol == ELEMENT:
+    #                 has_element = True
+    #                 lncrd = atom.coordinates
+    #                 lncrds.append(lncrd)
+    #     for com in asy_cry_name.components:
+    #         if any(atom.atomic_symbol == ELEMENT for atom in com.atoms): continue 
+    #         if has_element:
+    #             for atom in com.atoms:
+    #                 if atom.atomic_symbol == 'N':
+                        
+    #                     if is_nitrate(atom):
+    #                         Ncrd = atom.coordinates #Ncrd.x, Ncrd.y, Ncrd.z
+    #                         Ncrds.append(Ncrd)
+    #     dis_list = distance_calculation(lncrds,Ncrds)
+    #     if len(dis_list)>0:
+    #         print(cry.identifier) 
+    #         print(dis_list)
+    #         for com in asy_cry_name.components:
+    #             print(com.atoms)
+
+    #     if countc ==3: break
+    #     countc += 1        
+    return 0
+
+def main():
+    start = process_time()
+    LIST_OF_ELEMENT = [
+        'La', 'Ce','Pr','Nd','Sm','Eu','Gd','Tb','Dy','Ho','Er','Tm','Yb','Lu'
+                    ]       
+    
+    folder_path = 'data/subset3'
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path) 
+        
+    for ELEMENT in LIST_OF_ELEMENT:
+
+        nitrate_num_and_distance_in_ss(ELEMENT)    
+
+    #processing time
+    end = process_time()
+    print('Running time: %s Seconds' % (end - start))
+
+if __name__ == "__main__":
+    main()
+
+
